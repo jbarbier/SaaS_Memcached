de43ad8a0f4b599dcc8399dc9ada416ccda545fc
Julien Barbier <write0@gmail.com>
2013-04-05 16:43:27 -0700

    Add full_title helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+
+  # Returns the full title on a per-page basis.
+  def full_title(page_title)
+    base_title = "Memcached SaaS | cs50"
+    if page_title.empty?
+      base_title
+    else
+      "#{page_title} | #{base_title}"
+    end
+  end
+
 end
