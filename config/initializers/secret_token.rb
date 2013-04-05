917258ce7e452731f9755db7748c9ab5bfe92d41
Julien Barbier <write0@gmail.com>
2013-04-05 16:01:25 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SaaSMemcached::Application.config.secret_token = 'a875ca780800f89d6f9cff0b9c91f6196add1a2add3ff0b36625b96710df6052967c3323d98877750707df8eec314c1d6ea77e5f8d8bd0500eb4b6389a528e54'
