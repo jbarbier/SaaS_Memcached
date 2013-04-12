da29483f4eda3231761afbde44c9d1301ff39647
Julien Barbier <write0@gmail.com>
2013-04-12 01:09:56 +0000

    Link web and docker

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,3 +1,7 @@
+require 'rubygems'
+require 'json'
+require 'digest/md5'
+
 class UsersController < ApplicationController
   before_filter :signed_in_user, only: [:show]
 
@@ -7,19 +11,21 @@ class UsersController < ApplicationController
 
   def show
     @user = current_user
+    @md5 = digest = Digest::MD5.hexdigest(@user.email)[0...11]
   end
 
   def create
     @user = User.new(params[:user])
+    @user.memcached = create_memcached_instance
     if @user.save
       sign_in @user
-      flash[:success] = "Welcome to the Sample App!"
-      redirect_to @user
+      flash[:success] = "Welcome!"
+      redirect_to me_path
     else
       render 'new'
     end
   end
-
+  
   private
 
   def signed_in_user
@@ -27,6 +33,15 @@ class UsersController < ApplicationController
       store_location
       redirect_to signin_url, notice: "Please sign in."
     end
+  end
+
+  def create_memcached_instance
+    docker_path = '/home/julien/docker-master/'
+    container_id = `#{docker_path}docker run -d -p 11211 jbarbier/memcached memcached -u daemon`
+    cmd = "#{docker_path}docker inspect #{container_id}"
+    json_infos = `#{cmd}`
+    i = JSON.parse(json_infos)
+    port = i["NetworkSettings"]["PortMapping"]["11211"]
   end
 
 end
